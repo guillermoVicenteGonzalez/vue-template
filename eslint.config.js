1e7f6a12df2c5520f5f338a049cc134d9bb4cc60
guillermoVicenteGonzalez <guillermovg@usal.es>
2024-11-19 12:37:59 +0100

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,92 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginCypress from 'eslint-plugin-cypress/flat'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVitest from "@vitest/eslint-plugin";
+import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import pluginCypress from "eslint-plugin-cypress/flat";
+import pluginVue from "eslint-plugin-vue";
 
 export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
+	{
+		name: "app/files-to-lint",
+		files: ["**/*.{ts,mts,tsx,vue}"],
+	},
 
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
+	{
+		name: "app/files-to-ignore",
+		ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+	},
 
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
-  
-  {
-    ...pluginCypress.configs.recommended,
-    files: [
-      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
-    ],
-  },
-  skipFormatting,
-]
+	...pluginVue.configs["flat/recommended"],
+	...vueTsEslintConfig(),
+
+	{
+		...pluginVitest.configs.recommended,
+		files: ["src/**/__tests__/*"],
+	},
+
+	{
+		...pluginCypress.configs.recommended,
+		files: [
+			"cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}",
+			"cypress/support/**/*.{js,ts,jsx,tsx}",
+		],
+	},
+
+	{
+		name: "custom vue rules",
+		rules: {
+			"vue/no-arrow-functions-in-watch": "error",
+			// "vue/no-template-key": "error",
+			"vue/define-emits-declaration": ["error", "type-based"],
+			"vue/define-props-declaration": ["error", "runtime"],
+			"vue/enforce-style-attribute": [
+				"error",
+				{
+					allow: ["scoped", "module"],
+				},
+			],
+			"vue/block-lang": [
+				"error",
+				{
+					script: {
+						lang: "ts",
+					},
+				},
+			],
+			"vue/block-order": [
+				"error",
+				{
+					order: ["template", ["script", "style"]],
+				},
+			],
+			"vue/component-api-style": ["error", ["script-setup"]],
+			"vue/max-template-depth": ["error", { maxDepth: 10 }],
+		},
+	},
+
+	{
+		name: "custom typescript rules",
+		rules: {
+			//rules for both ts and vue files
+			"@typescript-eslint/array-type": "error",
+			"@typescript-eslint/class-methods-use-this": "error",
+			//rules for vue
+			"vue/no-unused-vars": "error",
+			//rules for typescript
+			"@typescript-eslint/consistent-type-definitions": "error",
+			"@typescript-eslint/no-empty-function": "error",
+			"@typescript-eslint/no-explicit-any": "error",
+			"@typescript-eslint/no-shadow": "error",
+			"@typescript-eslint/array-type": "error",
+			"@typescript-eslint/no-unused-expressions": "error",
+			"@typescript-eslint/naming-convention": [
+				"error",
+				{ selector: "variableLike", format: ["camelCase"] },
+			],
+			//to enforce naming for specific variable types
+
+			//booleans have verb is
+		},
+		files: ["**/*.{ts,mts,tsx,vue}"],
+	},
+	skipFormatting,
+];
